4f4a53b2a0f68a793d8d0d4f8615ec8507dfa08d
rnjogu205 <103487263+rnjogu205@users.noreply.github.com>
2025-09-11 22:03:54 -0300

    Added products to both men and women pages

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import './signup.css'
 
 const Signup= () =>{
     return(
-        <div>
+        <div className='signup'>
+            <div className="signup-container">
+                <h1>Sign Up</h1>
+                <div className="signup-fields">
+                    <input type="text" placeholder="Your Name"/>
+                    <input type="email" placeholder="Email Address"/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+                <button>Continue</button>
+                <p className="signup-signup">Already have an account? <span>Login here</span></p>
+                <div className="signup-agree">
+                    <input type="checkbox" name='' id='' />
+                    <p>By continuing i agree to the terms of use & privacy policy.</p>
+                </div>
+            </div>
             
         </div>
     );
