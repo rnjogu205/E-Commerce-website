0111f472e5f1581812726660b1c74869c2c3c374
rnjogu205 <103487263+rnjogu205@users.noreply.github.com>
2025-09-23 12:02:06 -0300

    Finished designing each item andd its attributes

--- src/Items/item.jsx
+++ src/Items/item.jsx
@@ -1,12 +1,13 @@
 import './Item.css'
+import {useCart} from '../Context/CartContext';
 
 function Item(props){
+    const {addToCart} = useCart();
     return(
         <div className="item-card">
             <img src={props.image} alt="" className="item-image"/>
             <h3 className="item-name">{props.name}</h3>
             <p className="item-price">${props.price} CAD</p>
-            <button className="add-btn">Add to Cart</button>
         </div>
     )
 }
