03e2a24f25b0ce96b653cfb4ef92a5a304a7f49d
rnjogu205 <103487263+rnjogu205@users.noreply.github.com>
2025-09-23 12:00:20 -0300

    modified navabr component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 import cartIcon from '../../assets/Cart.png';
 import {Link} from 'react-router-dom';
+import { CartContext } from '../../Context/CartContext';
 
 const Navbar = () => {
-
+  const {cart} = useContext(CartContext);
+  const cartCount = cart.reduce((acc, item) => acc + item.quantity, 0);
   return (
     <nav className="navbar">
       <div className="nav-logo">
@@ -22,8 +24,11 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <Link to='/login'><button className="login-btn">Log in</button></Link>
         <div className="cart-container">
-          <Link to='/cart'><img src={cartIcon} alt="cart" className="cart-icon" /></Link>
-          <div className="cart-count">0</div>
+          <Link to='/cart'>
+            <img src={cartIcon} alt="cart" className="cart-icon" />
+            <span className="cart-count">{cartCount}</span>
+          </Link>
+          
         </div>
       </div>
     </nav>
