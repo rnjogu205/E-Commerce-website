08f6cd813e475c937f599a399c7e549cefaeaa6d
rnjogu205 <103487263+rnjogu205@users.noreply.github.com>
2025-09-23 12:04:05 -0300

    Added a linear gradient background to the mens page

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -1,24 +1,32 @@
 import './men.css'
 import product_data from '../assets/All_data'
 import Item from '../Items/item.jsx'
+import { Link } from 'react-router-dom'
 
-const Men = ({category}) => {
+const Men = () => {
+  const filteredProducts = product_data.filter(product => product.category === 'men');
 
-    const filteredProducts = product_data.filter(product => product.category === 'men');
-    return(
-        <div className="men">
-            <h1>Mens</h1>
-            <hr/>
-            <div className="men-item-grid">
-                {filteredProducts.map((item)=> {
-                return <Item 
-                key={item.id} 
-                id={item.id} 
-                name={item.name}
-                image={item.image} 
-                price={item.price}/>})}
-            </div>
-        </div>
-    )
+  return (
+    <div className="men">
+      <h1>Mens</h1>
+      <hr />
+      <div className="men-item-grid">
+        {filteredProducts.map((item) => (
+          <Link
+            to={`/product/${item.id}`}
+            key={item.id}
+            style={{ textDecoration: "none", color: "inherit" }}>
+            <Item
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
-export default Men
+
+export default Men;
